f74c8978f4347bce539d0aa1373ce5667f1ce38a
Nikolay Fomenko <fomen_94@bk.ru>
2023-01-20 15:09:36 +0300

    feach: подключение всех шрифтов

--- src/components/AboutTeam/AboutTeam.js
+++ src/components/AboutTeam/AboutTeam.js
@@ -1,21 +1,30 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import Cart from '../../images/cart_1.svg'
 import Peoples from '../../images/peoples.svg'
+import Arrow from '../../images/Arrow_3.svg'
 import './AboutTeam.scss'
 
 const AboutTeam = () => {
+  const navigate = useNavigate();
 
   return (
     <section className='about-team'>
-      <div className='about-team__container'>
-      </div>
-        <ul className='about-team__list'>
-          <h2 className='about-team__list_title'>список команды</h2>
-        </ul>
-        <img className='about-team__img_cart' src={Cart} alt='тележка' />
-        <img className='about-team__img_peoples' src={Peoples} alt='люди' />
+      <button className='about-team__button'>
+        <Link className='about-team__link' to={() => navigate(-1)}>
+          <img className='about-team__arrow' src={Arrow} alt='стрелка' />
+          <span>
+            О проекте
+          </span>
+        </Link>
+      </button>
+      <ul className='about-team__list'>
+        <h2 className='about-team__list_title'>список команды</h2>
+      </ul>
+      <img className='about-team__img_cart' src={Cart} alt='тележка' />
+      <img className='about-team__img_peoples' src={Peoples} alt='люди' />
     </section>
-  );
+    );
 }
 
 export default AboutTeam;
