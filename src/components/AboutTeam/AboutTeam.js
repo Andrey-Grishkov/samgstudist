7f3caf1439616f4cf91fcd496a50dc02641fcb65
Nikolay Fomenko <fomen_94@bk.ru>
2023-01-20 12:20:58 +0300

    hotfix: рефакторинг about-team

--- src/components/AboutTeam/AboutTeam.js
+++ src/components/AboutTeam/AboutTeam.js
@@ -8,12 +8,12 @@ const AboutTeam = () => {
   return (
     <section className='about-team'>
       <div className='about-team__container'>
+      </div>
         <ul className='about-team__list'>
           <h2 className='about-team__list_title'>список команды</h2>
         </ul>
         <img className='about-team__img_cart' src={Cart} alt='тележка' />
         <img className='about-team__img_peoples' src={Peoples} alt='люди' />
-      </div>
     </section>
   );
 }
