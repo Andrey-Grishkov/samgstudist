3c4c42dd163c5f659454975830f87373b48c318c
Андрей Гришков <andreygorm@gmail.com>
2023-01-24 13:14:12 +0300

    ref page work preview

--- src/components/MainPageWorkPreview/MainPageWorkPreview.js
+++ src/components/MainPageWorkPreview/MainPageWorkPreview.js
@@ -6,6 +6,7 @@ import wave from "../../images/main-page-work-preview__image-wave.svg";
 import triangle from "../../images/main-page-work-preview__triangle.svg";
 import triangle2 from "../../images/main-page-work-preview__triangle-2.svg";
 import {theme} from '../../utils/constants'
+import arrow from "../../images/main-page-work-preview__arrow.svg";
 import Commercial from "../Commercial/Commercial";
 
 const MainPageWorkPreview = () => {
@@ -22,6 +23,23 @@ const MainPageWorkPreview = () => {
             <Commercial/>
           </div>
           <h3 className='main-page-work-preview__title'>{theme[0]}</h3>
+          <div className='main-page-work-preview__preview'>preview</div>
+          <div className='main-page-work-preview__nav-container'>
+            <div className='main-page-work-preview__page-btn-container'>
+              <button className='main-page-work-preview__btn'>
+                <img className='main-page-work-preview__arrow' src={arrow} alt='стрелка'/>
+                <p className='main-page-work-preview__btn-text'>Предыдущая страница</p>
+              </button>
+              <div className='main-page-work-preview__page-number'>4</div>
+              <button className='main-page-work-preview__btn'>
+                <img className='main-page-work-preview__arrow main-page-work-preview__arrow_reversed' src={arrow} alt='стрелка'/>
+                <p className='main-page-work-preview__btn-text main-page-work-preview__btn-text_reversed'>Следующая страница</p>
+              </button>
+            </div>
+            <button className='main-page-work-preview__btn-toggle-view'>Иллюстрации</button>
+
+
+          </div>
 
 
         </div>
