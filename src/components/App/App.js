19c108eb23870f8ab8c188e177916d4e3ad04e23
Nikolay Fomenko <fomen_94@bk.ru>
2023-01-25 23:31:47 +0300

    закончить страницу О проекте

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Main from '../Main/Main';
-import MainPageWorksList from '../MainPageWorksList/MainPageWorksList';
-import MainPageWorkPreview from '../MainPageWorkPreview/MainPageWorkPreview';
+import React, { useRef } from 'react'
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import MainPageWorksList from '../MainPageWorksList/MainPageWorksList'
+import MainPageWorkPreview from '../MainPageWorkPreview/MainPageWorkPreview'
 import './App.scss'
-import {Route, Routes, useLocation} from "react-router-dom";
-import AboutProject from "../AboutProject/AboutProject";
-import AboutTeam from "../AboutTeam/AboutTeam";
+import { Route, Routes, useLocation } from "react-router-dom"
+import AboutProject from "../AboutProject/AboutProject"
+import AboutTeam from "../AboutTeam/AboutTeam"
 
 function App() {
   const location = useLocation();
+  const inputRef = useRef(null);
+  const inputFocus = () => inputRef.current.focus();
 
   return (
     <div className='page'>
-        <Header/>
+      <Header
+        inputRef={inputRef}
+      />
       <Routes>
         <Route exact path='/' element={
-          <Main/>
+          <Main />
         }>
         </Route>
         <Route exact path='/works-list' element={
-          <MainPageWorksList/>
+          <MainPageWorksList />
         }>
         </Route>
         <Route exact path='/work-preview' element={
-          <MainPageWorkPreview/>
+          <MainPageWorkPreview />
         }>
         </Route>
         <Route exact path='/about-project' element={
-          <AboutProject/>
+          <AboutProject
+            inputFocus={inputFocus}
+          />
         }>
         </Route>
         <Route exact path='/about-team' element={
-          <AboutTeam/>
+          <AboutTeam />
         }>
         </Route>
       </Routes>
