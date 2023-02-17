6a59a23e8e4e676eda1fa3cdc687e12d72eb320c
Андрей Гришков <andreygorm@gmail.com>
2023-02-17 21:55:06 +0300

    ref search

--- src/components/WorkPreview/WorkPreview.js
+++ src/components/WorkPreview/WorkPreview.js
@@ -3,13 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchWork } from "../../utils/MainApi";
 import { setLimit } from "../../store/namberPageSlice";
+import "./WorkPreview.scss";
 
 export const WorkPreview = () => {
   const dispatch = useDispatch();
   const numberPage = useSelector((state) => state.namberPage.counter);
   const { id, workId } = useParams();
-
   const [text, setText] = useState("");
+
+
+  const [quantitySymbol, setQuantitySymbol] = useState(window.innerWidth < 721 ? 200 :
+    (window.innerWidth < 1109 ? 1200 : 1300));
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 721) {
+      setQuantitySymbol(200);
+    } else if (window.innerWidth < 1109) {
+      setQuantitySymbol(1200);
+    } else {
+      setQuantitySymbol(1300);
+    }});
 
   const fetchData = useCallback(async () => {
     const results = await fetchWork(id, workId);
@@ -20,7 +33,7 @@ export const WorkPreview = () => {
     const brokenText = [""];
     let counter = 0;
     for (let i = 0; i < paragraphTexts.length; i++) {
-      if (brokenText[counter].length < 1800) {
+      if (brokenText[counter].length < quantitySymbol) {
         brokenText[counter] += `${paragraphTexts[i]} `;
       } else {
         brokenText[counter] += "...";
@@ -36,5 +49,5 @@ export const WorkPreview = () => {
     fetchData();
   }, [fetchData]);
 
-  return <span>{text[numberPage]}</span>;
+  return <span className='work-preview'>{text[numberPage]}</span>;
 };
